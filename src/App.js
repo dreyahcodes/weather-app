ebe8415af00802c2c2cfbfb28d233aecc058e939
Dreyah <dreyah@Dreyahs-MacBook-Pro-2.local>
2019-03-06 03:42:42 +0100

    input weather API key, fetch data, create input form component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "f1749f4749ee185e386e38a9ed161165";
+
 class App extends React.Component {
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
