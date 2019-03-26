c23ea981cf7f58c4d355b15c333c7a600190714c
Dreyah <dreyah@Dreyahs-MacBook-Pro-2.local>
2019-03-26 22:52:54 +0100

    add responsiveness

--- src/App.js
+++ src/App.js
@@ -2,6 +2,101 @@ import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
+import styled from "styled-components";
+import backgroundImage from "./img/bg.jpg";
+
+const AppEl = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Merriweather:100,200,300,300i,400,400i,700");
+  @import url("https://fonts.googleapis.com/css?family=Roboto+Slab:400,700");
+  @import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400");
+
+  body {
+    font-family: "Open Sans", serif;
+  }
+
+  .wrapper {
+    background: linear-gradient(to right, #e67e22, #e74c3c);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main {
+    height: 90vh;
+    background: #283e51;
+    box-shadow: 0px 13px 40px -13px rgba(0, 0, 0, 0.75);
+    /* width: 80%; */
+    margin: 0 auto;
+  }
+
+  .title-container {
+    height: 90vh;
+    background: url(${backgroundImage}) center center no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #000;
+  }
+
+  .form-container {
+    background-color: #2c3e50;
+    height: 90vh;
+    padding-top: 100px;
+    padding-left: 50px;
+  }
+
+  .weather__info {
+    width: 60%;
+    font-size: 20px;
+    font-weight: 200;
+    letter-spacing: 2px;
+  }
+
+  .weather__key {
+    color: #f16051;
+    border-bottom: solid 2px rgba(255, 255, 255, 0.06);
+    padding: 20px 0 20px 0;
+    font-weight: 400;
+  }
+
+  .weather__key:last-child {
+    border: 0;
+  }
+
+  .weather__value {
+    color: #fff;
+    font-weight: 200;
+  }
+
+  .weather__error {
+    color: #f16051;
+    font-size: 20px;
+    letter-spacing: 1px;
+    font-weight: 200;
+  }
+
+  @media only screen and (max-width: 776px) {
+    .title-container {
+      width: 100%;
+    }
+    .form-container {
+      width: 100%;
+    }
+
+    .wrapper {
+      height: auto;
+      padding: 30px 0;
+
+      .main {
+        margin: 0 20px;
+        height: auto;
+      }
+    }
+  }
+`;
 
 const API_KEY = "f1749f4749ee185e386e38a9ed161165";
 
@@ -45,7 +140,7 @@ class App extends React.Component {
   };
   render() {
     return (
-      <div>
+      <AppEl>
         <div className="wrapper">
           <div className="main">
             <div className="container">
@@ -68,7 +163,7 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </AppEl>
     );
   }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,68 @@
 import React from "react";
+import styled from "styled-components";
+
+const FormEl = styled.div`
+  input[type="text"] {
+    background-color: transparent;
+    border: 0;
+    border-bottom: solid 1px #f16051;
+    width: 30%;
+    padding-bottom: 4px;
+    color: #fff !important;
+    font-weight: lighter;
+    letter-spacing: 2px;
+    margin-bottom: 30px;
+    margin-right: 20px;
+    font-size: 20px;
+  }
+
+  input[type="text"] {
+    outline: none;
+  }
+
+  input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 30px #2c3e50 inset;
+    -webkit-text-fill-color: #fff !important;
+  }
+
+  button {
+    border: 0;
+    padding: 8px 20px;
+    margin: 0 2px;
+    border-radius: 2px;
+    font-weight: lighter;
+    letter-spacing: 1px;
+    font-size: 15px;
+    cursor: pointer;
+    background-color: #f16051;
+    color: #fff;
+    font-weight: 400;
+  }
+
+  button:active {
+    outline: none;
+  }
+
+  @media only screen and (max-width: 990px) {
+    input[type="text"] {
+      width: 37%;
+    }
+  }
+  @media only screen and (max-width: 880px) {
+    input[type="text"] {
+      width: 60%;
+    }
+  }
+`;
 
 const Form = props => (
-  <form onSubmit={props.getWeather}>
-    <input type="text" name="city" placeholder="city..." />
-    <input type="text" name="country" placeholder="country..." />
-    <button>Get Weather</button>
-  </form>
+  <FormEl>
+    <form onSubmit={props.getWeather}>
+      <input type="text" name="city" placeholder="city..." />
+      <input type="text" name="country" placeholder="country..." />
+      <button>Get Weather</button>
+    </form>
+  </FormEl>
 );
 
 export default Form;
